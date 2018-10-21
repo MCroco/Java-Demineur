7546f2281b02af4d58e00526c2d15561c8c36649
KodiOko <44288150+KodiOko@users.noreply.github.com>
2018-10-21 18:46:05 +0200

    Update1.0
    Affiche dans une fenêtre les données

---
+++ Fenetre.java
@@ -0,0 +1,29 @@
+package first;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class Fenetre extends JFrame {
+
+	public Fenetre(){
+	    this.setLocationRelativeTo(null);
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    this.setTitle("JTable");
+	    this.setSize(500, 375);
+
+	    //Les données du tableau
+	    Tableau tab = new Tableau(4, 4, 3);
+	    Object [][] data = tab.getTab();
+
+	    //Les titres des colonnes
+	    String  title[] = {"", "", "", ""};
+	    JTable tableau = new JTable(data, title);
+	    this.getContentPane().add(new JScrollPane(tableau));
+	  }
+
+	  public static void main(String[] args){
+	    Fenetre fen = new Fenetre();
+	    fen.setVisible(true);
+	  }   
+}
