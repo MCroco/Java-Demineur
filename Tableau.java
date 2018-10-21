465095876caffa0a53f3f626dcc8cb3f1ec269aa
KodiOko <44288150+KodiOko@users.noreply.github.com>
2018-10-21 18:16:06 +0200

    Update Tableau
    Je n'avais pas vu qu'il y avait un problème lors de la création du tableau avec les calculs de présence de bombe ou non.
    Mais maintenant c'est réglé.
    Et vérifier cette fois-ci hahaha

--- Tableau.java
+++ Tableau.java
@@ -1,19 +1,24 @@
 package first;
 
 public class Tableau {
-	private int [][] tab;
+	private String [][] tab;
 	private Position [] pos;
 	
 	public Tableau(int lignes, int colonnes, int nbrBomb) {
+		pos = new Position[nbrBomb];
 		for (int i = 0; i<nbrBomb; i++) {
 			pos[i] = new Position(lignes, colonnes);
 		}
-		this.tab = new int[lignes][colonnes];
+		this.tab = new String[lignes][colonnes];
 		remplissageBomb();
+		remplissageTableau();
 	}
 	
 	public boolean isBomb(int li, int co) {
-		if (tab[li][co] == -1) {
+		if (tab[li][co] == null) {
+			return false;
+		}
+		if (tab[li][co].equals("-1")) {
 			return true;
 		}
 		return false;
@@ -22,7 +27,7 @@ public class Tableau {
 	public void remplissageBomb() {
 		for (int i=0; i<pos.length;i++) {
 			Position temp = pos[i];
-			tab[temp.getLigne()][temp.getColonne()] = -1;
+			tab[temp.getLigne()][temp.getColonne()] = "-1";
 		}
 	}
 	
@@ -31,38 +36,40 @@ public class Tableau {
 		for (int i=0; i<tab.length; i++) {
 			for (int j=0; j<tab[i].length; j++) {
 				count = 0;
-				if (i==0) {
-					if (j==0) {
-						count += coinHautGauche(i, j);
+				if (!isBomb(i, j)) {
+					if (i==0) {
+						if (j==0) {
+							count += coinHautGauche(i, j);
+						}
+						else if (j == tab[i].length-1) {
+							count += coinHautDroit(i, j);
+						}
+						else {
+							count += millieuHorizontalHaut(i, j);
+						}
 					}
-					else if (j == tab[i].length) {
-						count += coinHautDroit(i, j);
+					else if (i==tab.length-1) {
+						if (j==0) {
+							count += coinBasGauche(i, j);
+						}
+						else if (j==tab[i].length-1) {
+							count += coinBasDroit(i, j);
+						}
+						else {
+							count += millieuHorizontalBas(i, j);
+						}
+					}
+					else if (j==0) {
+						count += millieuVerticalGauche(i, j);
+					}
+					else if (j==tab[i].length-1) {
+						count += millieuVerticalDroit(i, j);
 					}
 					else {
-						count += millieuHorizontalHaut(i, j);
+						count += millieu(i, j);
 					}
+					tab[i][j] = Integer.toString(count);
 				}
-				else if (i==tab.length) {
-					if (j==0) {
-						count += coinBasGauche(i, j);
-					}
-					else if (j==tab[i].length) {
-						count += coinBasDroit(i, j);
-					}
-					else {
-						count += millieuHorizontalBas(i, j);
-					}
-				}
-				else if (j==0) {
-					count += millieuVerticalGauche(i, j);
-				}
-				else if (j==tab[i].length) {
-					count += millieuVerticalDroit(i, j);
-				}
-				else {
-					count += millieu(i, j);
-				}
-				tab[i][j] = count;
 			}
 		}
 	}
@@ -222,5 +229,22 @@ public class Tableau {
 			countMillieux++;
 		}
 		return countMillieux;
+	}
+
+	@Override
+	public String toString() {
+		String temp= "";
+		for (int i=0; i<tab.length; i++) {
+			temp += "\n";
+			for (int j=0; j<tab[i].length; j++) {
+				temp += "[" + i + "] [" + j + "] : " + tab[i][j] + " ";
+			}
+		}
+		return temp;
+	}
+	
+	public static void main(String[] args) {
+		Tableau tab = new Tableau(4, 4, 2);
+		System.out.println(tab.toString());
 	}
 }
