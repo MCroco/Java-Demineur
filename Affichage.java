b9b4bddecee4dfbbc64449311753173aa19f88ff
guivdh <32937851+guivdh@users.noreply.github.com>
2018-11-27 21:39:43 +0100

    Affichage.java
    algo qui dit si on a cliquer sur une bombe + retourne toutes les bombes
    algo qui retourne SEULEMENT toutes les cases autour d'une sélectionnée (sauf si bombe)

--- Affichage.java
+++ Affichage.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 public class Affichage {
 	
+	private static int ligne;
+	private static int colonne;
+	static boolean verif2 = false;
+	
 	public static void devoilement(Tableau aff, Tableau tab, Scanner sc) {
-		System.out.println("Entrez les coordonnées de la case à retourner (Ligne;Colonne)");
+		System.out.println(("Entrez les coordonnÃ©es de la case Ã  retourner (Ligne(max = "+ligne+");Colonne(max = " + colonne + ")"));
 		String rep = sc.nextLine();
 		String temp[] = rep.split(";");
 		
@@ -13,10 +17,52 @@ public class Affichage {
 		int y = Integer.parseInt(temp[1]) - 1;
 		
 		String value = tab.getCase(x, y);
+		System.out.println(value);
+
 		
-		aff.setCase(x, y, value);
+		if(value.equals("%")) {
+			
+			System.out.println("Perdu!");
+			verif2=true;
+			for(int i=0; i<ligne;i++) {
+				for(int j=0; j<colonne;j++) {
+					if(tab.getCase(i, j).equals("%")) {
+
+						String value2 = tab.getCase(i, j);
+						aff.setCase(i, j, value2);
+					}
+				}
+			}
+			System.out.println(aff);
+		}
+		else if(value.equals("0")){
+	
+					
+			algoRetrun(aff, tab, x, y);
+			
+		}
+		else {
+			
+			aff.setCase(x, y, tab.getCase(x, y));
+		}
+		
 		System.out.println(aff);
-	}	
+		
+		
+}
+	
+	public static void algoRetrun(Tableau aff, Tableau tab, int x, int y) {
+		for(int i=-1; i<2; i++) {
+			
+			for(int j=-1; j<2; j++) {
+				
+				if(tab.getCase(x+i, y+j).equals("0")) {
+					aff.setCase(x+i, y+j, " ");
+				}
+				else aff.setCase(x+i, y+j, tab.getCase(x+i, y+j));
+			}
+		}
+	}
 	
 	public static void main(String [] args) {
 		Tableau tab = new Tableau(0, 0, 0);
@@ -25,7 +71,7 @@ public class Affichage {
 		
 		boolean verif = false;
 		while (!verif) {
-			System.out.println("Quel niveau de difficulté choisissez-vous ? Facile | Moyen | BADASSE | Exit");
+			System.out.println("Quel niveau de difficultÃ© choisissez-vous ? Facile | Moyen | BADASSE | Exit");
 			String resultat = sc.nextLine();
 			
 		
@@ -34,6 +80,8 @@ public class Affichage {
 				tabVide = new Tableau(10, 15, 0);
 				System.out.println(tab.toString());
 				System.out.println(tabVide.toString());
+				ligne = 10;
+				colonne = 15;
 				verif = true;
 			}
 			else if(resultat.equalsIgnoreCase("Moyen")){
@@ -41,6 +89,8 @@ public class Affichage {
 				tabVide = new Tableau(20, 30, 0);
 				System.out.println(tab.toString());
 				System.out.println(tabVide.toString());
+				ligne = 20;
+				colonne = 30;
 				verif = true;
 			}
 			else if(resultat.equalsIgnoreCase("BADASSE")){
@@ -48,18 +98,22 @@ public class Affichage {
 				tabVide = new Tableau(30, 50, 0);
 				System.out.println(tab.toString());
 				System.out.println(tabVide.toString());
+				ligne = 30;
+				colonne = 35;
 				verif = true;
 			}
 			else if(resultat.equalsIgnoreCase("Exit")) {
 				break;
 			}
 			else {
-				System.out.println("Vous n'avez entré aucune des possibilités proposées.");
+				System.out.println("Vous n'avez entrÃ© aucune des possibilitÃ©s proposÃ©es.");
 			}
 		}
-		boolean verif2 = false;
 		while(!verif2) {
+			
+
 			devoilement(tabVide, tab, sc);
+			
 		}
 	}
 }
