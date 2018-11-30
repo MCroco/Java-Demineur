24eb433b9eb19a04c4c851a032d61ab8be028d59
Mirica Constantin <32748526+MCroco@users.noreply.github.com>
2018-11-30 10:12:24 +0100

    Add files via upload

---
+++ package-info.java
@@ -0,0 +1,16 @@
+/**
+ * This package contains all the classes needed by our project.
+ * <ul>
+ * <li>The class <code> Affichage.java</code> </li>
+ * <li>The class <code> Position.java</code> </li>
+ * <li>The class <code> Tableau.java</code> </li>
+ * <li>The class <code> Fenetre.java</code> </li>
+ * 
+ * @author Group 14 - 2TL2
+ * @author HENRY Nathan
+ * @author VANDEN  HERREWEGEN Guillaume 
+ * @author MIRICA Constantin
+ *  
+ *
+ */
+package first;
